2988b9c28f4ad1bef3f6d95d4d0b42c2d51195ef
desireUjjwal12 <ineffableujjwal@gmail.com>
2025-09-09 21:14:46 +0530

    Create Demo1_DataTypes.java

---
+++ core_java/Demo1_DataTypes.java
@@ -0,0 +1,8 @@
+class Demo1_DataTypes{
+    public static void main(String[] args) {
+        long l = 1000_00l;
+        char c = 'a';
+        System.out.println(c + 1); 
+        System.out.println(l);
+    }
+}
