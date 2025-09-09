86fb45802b21354480e29dc61a1ba56c6f684e21
desireUjjwal12 <ineffableujjwal@gmail.com>
2025-09-09 21:45:43 +0530

    Update Demo3_TypeConversion.java

--- core_java/Demo3_TypeConversion.java
+++ core_java/Demo3_TypeConversion.java
@@ -6,6 +6,11 @@ public class Demo3_TypeConversion {
     int i = (int) f; // explicit casting, decimal part will be truncated
     double d = f; // implicit casting, float to double
 
+    byte num1 = 10;
+    byte num2 = 30;
+
+    int result = num1 * num2; // implicit casting, byte to int during multiplication (Type Promotion)
+
     public static void main(String[] args) {
         Demo3_TypeConversion obj = new Demo3_TypeConversion();
         System.out.println("Value of a: " + obj.a);
@@ -13,5 +18,6 @@ public class Demo3_TypeConversion {
         System.out.println("Value of f: " + obj.f);
         System.out.println("Value of i (after casting f to int): " + obj.i);
         System.out.println("Value of d (after implicit casting f to double): " + obj.d);
+        System.out.println("Result of num1 * num2 (after implicit casting byte to int): " + obj.result);
     }
 }
