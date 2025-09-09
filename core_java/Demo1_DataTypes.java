608f1554a3238f7352304a2bc3de2a9b1a941480
desireUjjwal12 <ineffableujjwal@gmail.com>
2025-09-09 21:18:47 +0530

    Update Demo1_DataTypes.java

--- core_java/Demo1_DataTypes.java
+++ core_java/Demo1_DataTypes.java
@@ -1,7 +1,12 @@
 class Demo1_DataTypes{
     public static void main(String[] args) {
+        int num1 = 9;
+        byte by = 127;
         long l = 1000_00l;
         char c = 'a';
+        float f = 5.8f;
+        double d = 9.7;
+        boolean b = true;
         System.out.println(c + 1); 
         System.out.println(l);
     }
