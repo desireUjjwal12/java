80e632ffd20772356ddf6a2e5cda269a4b3d2f39
desireUjjwal12 <ineffableujjwal@gmail.com>
2025-09-09 21:51:45 +0530

    Create Demo5_RelationalOperators.java

---
+++ core_java/Demo5_RelationalOperators.java
@@ -0,0 +1,15 @@
+public class Demo5_RelationalOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        //  Relational operations
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a == b : " + (a == b));  // equal to
+        System.out.println("a != b : " + (a != b));  // not equal to
+        System.out.println("a > b  : " + (a > b));   // greater than
+        System.out.println("a < b  : " + (a < b));   // less than
+        System.out.println("a >= b : " + (a >= b));  // greater than or equal to
+        System.out.println("a <= b : " + (a <= b));  // less than or equal to
+    }
+}
